dedae5b30b1670f1c0ae795ecea0d87ed0e0e7c8
forsilence <3312034934@qq.com>
2023-04-24 19:59:19 +0800

    event dispatcher

--- GameEngine/Event.h
+++ GameEngine/Event.h
@@ -50,4 +50,29 @@ namespace GameEngine {
 		bool m_Handled = false;
 	};
 
+	class EventDispatcher
+	{
+		template<typename T> 
+		using EventFn = std::function<bool>(T&) > ;
+	public:
+		EventDispatcher(Event& event): m_Event(event){}
+		template<typename T>
+		bool Dispatch(EventFn<T> fn)
+		{
+			if (m_Event.GetEventType() == T::GetStaticType())
+			{
+				m_Event.m_Handled = fn(*(T*)&m_Event);
+				return true;
+			}
+			return false;
+		}
+	private:
+		Event& m_Event;
+	};
+
+	inline std::ostream& operator<<(std::ostream& os, const Event& e) 
+	{
+		return os << e.ToString();
+	}
+
 }
